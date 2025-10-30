e15a0a9f084aeda7bfeda0249ceff450b04590db
arthur <arthur.rios007@gmail.com>
2025-10-29 23:02:58 -0300

    feat: update total calculation in QuoteCard to include discount, improving accuracy of displayed price

--- src/components/QuoteCard/index.tsx
+++ src/components/QuoteCard/index.tsx
@@ -15,17 +15,19 @@ interface QuoteCardProps extends TouchableOpacityProps {
 }
 
 export function QuoteCard({ quote, ...props }: QuoteCardProps) {
-  const total = quote.items.reduce(
+  const totalWithoutDiscount = quote.items.reduce(
     (acc, item) => acc + item.price * item.qty,
     0,
   )
+  const totalWithDiscount =
+    totalWithoutDiscount * (1 - (quote.discountPct ?? 0) / 100)
   return (
     <TouchableOpacity style={styles.container} {...props}>
       <View style={{ gap: 8, flexShrink: 1 }}>
         <Text style={styles.quoteTitle}>{quote.title}</Text>
         <Text style={styles.quoteClient}>{quote.client}</Text>
       </View>
-      <MoneyLabel value={total} size="lg" />
+      <MoneyLabel value={totalWithDiscount} size="lg" />
       <Status status={statusMapping[quote.status]} style={styles.status} />
     </TouchableOpacity>
   )
